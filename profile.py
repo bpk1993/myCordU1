73b2146256105e6733277fde462bb46765d7b48a
Pradeep K <bpradeepkumar.24@gmail.com>
2017-08-30 00:46:13 -0600

    updated shell value

--- profile.py
+++ profile.py
@@ -34,7 +34,7 @@ apool = IG.AddressPool("nm",2)
 request.addResource(apool)
 
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+node.addService(pg.Execute(shell="bash", command="/local/repository/silly.sh"))
 
 # Print the generated rspec
 pc.printRequestRSpec(request)
